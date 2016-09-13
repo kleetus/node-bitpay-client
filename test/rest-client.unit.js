11508d9eee5b8ecb8d299f18868e05cf505395d7
Chris Kleeschulte <chrisk@bitpay.com>
2016-09-13 10:11:22 -0400

    wip

--- test/rest-client.unit.js
+++ test/rest-client.unit.js
@@ -6,12 +6,19 @@ var myPrivateKey = '97811b691dd7ebaeb67977d158e1da2c4d3eaa4ee4e2555150628acade6b
 
 describe('RESTClient', function() {
 
-  var RESTClient = proxyquire('../lib/rest-client', {
-    'request': sinon.stub().returnsArg(0).callsArgWithAsync(1, null, {
+  var isWin = (process.platform === 'win32' || process.platform === 'win64');
+  var HOME = process.env[isWin ? 'USERPROFILE' : 'HOME'];
+  var configFile = HOME + '/.bitpay/config.json';
+  var stubs = {};
+  stubs.request = sinon.stub().returnsArg(0).callsArgWithAsync(1, null, {
       statusCode: 200
-    }, '{ "data": [{}] }')
-  });
-
+    }, '{ "data": [{}] }');
+  stubs[configFile] = {
+    apiHost: 'fileApiHost',
+    apiPort: '7777',
+    forceSSL: 'true', '@noCallThru': true
+  };
+  var RESTClient = proxyquire('../lib/rest-client', stubs);
   describe('#new', function() {
 
     it('should create an instance of the client without a secret', function(done) {
@@ -41,6 +48,37 @@ describe('RESTClient', function() {
       done();
     });
 
+    it('should use environment variables for default configuration, if provided', function() {
+      process.env.BITPAY_API_HOST = 'envApiHost';
+      process.env.BITPAY_API_PORT = '9999';
+      var client = new RESTClient( myPrivateKey );
+      client.apiHost.should.equal('envApiHost');
+      client.apiPort.should.equal(9999);
+      delete process.env.BITPAY_API_HOST;
+      delete process.env.BITPAY_API_PORT;
+    });
+
+    it('should use configuration file if no environment variables are specified', function() {
+      var client = new RESTClient( myPrivateKey );
+      client.apiHost.should.equal('fileApiHost');
+      client.apiPort.should.equal('7777');
+    });
+
+    it('should use the hard-coded test values if environment variable set for this', function() {
+      process.env.USE_BITPAY_TESTNET = 'true';
+      var client = new RESTClient( myPrivateKey );
+      client.apiHost.should.equal('test.bitpay.com');
+      client.apiPort.should.equal(443);
+      delete process.env.USE_BITPAY_TESTNET;
+    });
+
+    it('should use a combination of environment variables and file parameters', function() {
+      process.env.BITPAY_API_PORT = 999;
+      var client = new RESTClient( myPrivateKey );
+      client.apiHost.should.equal('fileApiHost');
+      client.apiPort.should.equal(999);
+      delete process.env.BITPAY_API_PORT;
+    });
   });
 
   describe('#as', function() {
